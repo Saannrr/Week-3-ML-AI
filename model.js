0f477dac2fdbe380f49bb1bc14e3e8c0c3fb5e68
Saannrr <ihsan.ican2002@gmail.com>
2021-11-20 15:42:48 +0700

    commit benerin

--- model.js
+++ model.js
@@ -10,8 +10,34 @@ function loadData() {
 }
 
 // Buat fungsi untuk load data disini
+async function loadModel(){
+  console.log('loading model....');
+  model = await tf.loadLayersModel(
+    'file://${__dirname}/model/model.json',
+    false
+  );
+  console.log('Model Loaded Successfull')
+};
 
 const movie_arr = tf.tensor(loadData());
 const movie_len = movies.length;
 
 // Buat fungsi untuk memberi rekomendasi film
+exports.recommend = async function recommend(userid){
+  let user = tf.fill([movie_len],Number(userid));
+  let movie_in_js_array = movie_arr.arraySync();
+  await loadModel();
+  console.log('Recommending for user: ${userId}');
+  pred_tensor = await model.predict([movie_arr,user]).reshape([movie_len]);
+  pred = pred.tensor.arraySyck();
+
+  let recommendations = [];
+  for (let i = 0;i<6;i++){
+    max = pred_tensor.argMax().arraySync();
+    recommendations.push(movies[max]);
+    pred.splice(max,1);
+    pred_tensor = tf.tensor(pred);
+  }
+  console.log(recommendations)
+  return recommendations
+}
